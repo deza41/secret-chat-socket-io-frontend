916133e2478138af81e2396a104e98a1c086ca25
deza41 <deza41@hotmail.com>
2021-07-23 16:28:49 +1000

    final touches

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,10 +1,34 @@
 const About = () => {
-    return <div className="container">
-        <div className="row">
-            <h1 className="text-center p-5 display-1">About</h1>
-            <hr></hr>
+
+    return(
+        <div className="container-fluid h-100">
+            <div className="row justify-content-center h-100">
+                <h1 className="text-center p-2 display-1">About</h1>
+                <hr></hr> 
+                <div className="">
+                    <div className="card-body msg_card_body" id='bottom'>
+                        <p className="text-center p-4">Secret chat app which uses socket.io to relay messages in real time to the backend server.</p>
+                        <p className="text-center">Backend technologies:</p>
+                        <ul className="Aboutcard">
+                            <li className="">Node.js</li>
+                            <li className="">Express.js</li>
+                            <li className="">Socket.io</li>  
+                        </ul>
+                        <br></br>
+                        <br></br>
+                        <p className="text-center p-2">Frontend technologies:</p>
+                        <ul className="Aboutcard2">
+                            <li className="">React.js</li>
+                            <li className="">Socket.io</li>  
+                        </ul>
+                    </div >
+                </div>
+            </div>
         </div>
-    </div>
+    
+    )
+        
+
 }
 
 export default About;
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -3,7 +3,9 @@ const Contact = () => {
         <div className="row">
             <h1 className="text-center p-5 display-1">Contact</h1>
             <hr></hr>
-
+            <p className="text-center p-5 display-6"><b>email:</b> deza41@hotmail.com</p>
+            <p className="text-center p-5 display-6"><b>Personal Website: </b>
+            <a href="https://www.tyronedeza.com/">Click Here</a> </p>
         </div>
     </div>
 }
