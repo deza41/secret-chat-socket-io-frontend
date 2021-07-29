c3850e4cd7435dfeb37956545954a9b7eaebb121
deza41 <deza41@hotmail.com>
2021-07-29 16:19:44 +1000

    pg add

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -37,18 +37,28 @@ const Home = () => {
 
         socket.emit('last-chat', "getting chat");
 
+        // const sweet = {"command":"SELECT","rowCount":5,"oid":null,"rows":[{"msg_id":11,"msg_log":"cat_man: yoyooyoyoy"},{"msg_id":12,"msg_log":"cat_man: yoyooyoyoy"},{"msg_id":13,"msg_log":"cat_man: dsdsdssd"},{"msg_id":14,"msg_log":"cat_man: dsdsdssd"},{"msg_id":15,"msg_log":"cat_man: sdsdsdsdsdsdsdd"}],"fields":[{"name":"msg_id","tableID":2886789,"columnID":1,"dataTypeID":23,"dataTypeSize":4,"dataTypeModifier":-1,"format":"text"},{"name":"msg_log","tableID":2886789,"columnID":2,"dataTypeID":1043,"dataTypeSize":-1,"dataTypeModifier":229,"format":"text"}],"_parsers":[null,null],"_types":{"_types":{"arrayParser":{},"builtins":{"BOOL":16,"BYTEA":17,"CHAR":18,"INT8":20,"INT2":21,"INT4":23,"REGPROC":24,"TEXT":25,"OID":26,"TID":27,"XID":28,"CID":29,"JSON":114,"XML":142,"PG_NODE_TREE":194,"SMGR":210,"PATH":602,"POLYGON":604,"CIDR":650,"FLOAT4":700,"FLOAT8":701,"ABSTIME":702,"RELTIME":703,"TINTERVAL":704,"CIRCLE":718,"MACADDR8":774,"MONEY":790,"MACADDR":829,"INET":869,"ACLITEM":1033,"BPCHAR":1042,"VARCHAR":1043,"DATE":1082,"TIME":1083,"TIMESTAMP":1114,"TIMESTAMPTZ":1184,"INTERVAL":1186,"TIMETZ":1266,"BIT":1560,"VARBIT":1562,"NUMERIC":1700,"REFCURSOR":1790,"REGPROCEDURE":2202,"REGOPER":2203,"REGOPERATOR":2204,"REGCLASS":2205,"REGTYPE":2206,"UUID":2950,"TXID_SNAPSHOT":2970,"PG_LSN":3220,"PG_NDISTINCT":3361,"PG_DEPENDENCIES":3402,"TSVECTOR":3614,"TSQUERY":3615,"GTSVECTOR":3642,"REGCONFIG":3734,"REGDICTIONARY":3769,"JSONB":3802,"REGNAMESPACE":4089,"REGROLE":4096}},"text":{},"binary":{}},"RowCtor":null,"rowAsArray":false}
+        // sweet.rows.map((message) => {
+        //     addResponseMessage(message.msg_log)
+        //     setRec({text:message.msg_log, user: "external"})
+
+        //     var objDiv = document.getElementById("bottom");
+        //     objDiv.scrollTop = objDiv.scrollHeight;
+
+
+        // })
+
         socket.on('get-chat', (chat)=>{
-            // console.log(users)
 
-            addResponseMessage(chat.row[0].msg_log);
-            //rec must be to store the message as the [] < dependancy requires the dependancy to not change size e.g. ...chat
-            //this is then passed into another use affect with the dependancy[rec] to ensure that it will only run when rec changes thus allowing us to change chat without callback stacks
-
-            // setRec({text:message, user: "external"})
-            // setchat([message]);
-
-            // var objDiv = document.getElementById("bottom");
-            // objDiv.scrollTop = objDiv.scrollHeight;
+            // chat.rows.map((message) => {
+            //     addResponseMessage(message.msg_log)
+            //     setRec({text:message.msg_log, user: "external"})
+    
+            //     var objDiv = document.getElementById("bottom");
+            //     objDiv.scrollTop = objDiv.scrollHeight;
+    
+    
+            // })
 
             console.log(chat)
         });
